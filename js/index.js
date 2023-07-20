d2d5e2b9e6ac8f8b035540d33cd487a3c28b79a4
Khoahello <dinhtankhoa2k7@gmail.com>
2023-07-21 03:32:18 +0700

    finish header

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
-document.getElementById("switchButton").onclick = function () {
+function turn_dark() {
     document.getElementById("myBody").classList.toggle("dark");
-  };
+}
+
+function disableTxt() {
+  document.getElementById("header_search").hidden = true;
+}
+
+function undisableTxt() {
+  document.getElementById("header_search").hidden = false;
+}
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+    if($(this).scrollTop()){
+      $("header").addClass("sticky")
+    } else {
+      $("header").removeClass("sticky")
+    }
+  })
+});
